e655e7b596a47ed1867d4d50dcae521a6f34676d
takayaogura <mail@takayaogura.com>
2021-05-27 07:14:20 +0900

    Initial Commit

---
+++ MyToDos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyToDos
+//
+//  Created by 小倉岳哉 on 2021/05/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyToDos/MyToDosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDosApp.swift
+//  MyToDos
+//
+//  Created by 小倉岳哉 on 2021/05/27.
+//
+
+import SwiftUI
+
+@main
+struct MyToDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
